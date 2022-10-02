6e085811977068cf65ec1505f6577cdd9a9b7152
Yousef Farge <yousef.farge@gmail.com>
2022-10-02 13:35:42 -0500

    Fetch data

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,22 @@
-import Banner from "./components/Banner/Banner";
-import CourseList from "./components/CourseList/CourseList";
-const schedule = {
-  title: "CS Courses for 2018-2019",
-  courses: {
-    F101: {
-      term: "Fall",
-      number: "101",
-      meets: "MWF 11:00-11:50",
-      title: "Computer Science: Concepts, Philosophy, and Connections",
-    },
-    F110: {
-      term: "Fall",
-      number: "110",
-      meets: "MWF 10:00-10:50",
-      title: "Intro Programming for non-majors",
-    },
-    S313: {
-      term: "Spring",
-      number: "313",
-      meets: "TuTh 15:30-16:50",
-      title: "Tangible Interaction Design and Learning",
-    },
-    S314: {
-      term: "Spring",
-      number: "314",
-      meets: "TuTh 9:30-10:50",
-      title: "Tech & Human Interaction",
-    },
-  },
-};
+import Banner from './components/Banner/Banner';
+import CourseList from './components/CourseList/CourseList';
+import { useJsonQuery } from './utilities/fetch';
 
-const App = () => (
-  <>
-    <Banner title={schedule.title} />
-    <CourseList courses={schedule.courses} />
-  </>
-);
+const App = () => {
+  const [data, isLoading, error] = useJsonQuery(
+    'https://courses.cs.northwestern.edu/394/guides/data/cs-courses.php'
+  );
+
+  if (error) return <h1>Error loading user data: {`${error}`}</h1>;
+  if (isLoading) return <h1>Loading user data...</h1>;
+  if (!data) return <h1>No user data found</h1>;
+
+  return (
+    <>
+      <Banner title={data.title} />
+      <CourseList courses={data.courses} />
+    </>
+  );
+};
 
 export default App;
