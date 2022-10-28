3183cd9e7c554962ea1d0ba3f58a34df0e2462ec
Yousef Farge <yousef.farge@gmail.com>
2022-10-27 23:15:20 -0500

    only authenticated users can view course edit button

--- src/components/Course/Course.jsx
+++ src/components/Course/Course.jsx
@@ -1,9 +1,11 @@
 import React, { useState } from 'react';
+import { useAuthState } from '../../utilities/firebase';
 import { hasConflict } from '../../utilities/times';
 import CourseForm from '../CourseForm/CourseForm';
 import './Course.css';
 
 const Course = ({ course, selected, setSelected }) => {
+  const [user] = useAuthState();
   const [open, setOpen] = useState(false);
   const openForm = () => setOpen(true);
   const closeForm = () => setOpen(false);
@@ -33,22 +35,24 @@ const Course = ({ course, selected, setSelected }) => {
         className="course-header"
       >
         CS {course.number}
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="16"
-          height="16"
-          fill="currentColor"
-          className="bi bi-pencil-square"
-          viewBox="0 0 16 16"
-          onClick={openForm}
-          id="course-edit"
-        >
-          <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z" />
-          <path
-            fillRule="evenodd"
-            d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z"
-          />
-        </svg>
+        {!!user && (
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="16"
+            height="16"
+            fill="currentColor"
+            className="bi bi-pencil-square"
+            viewBox="0 0 16 16"
+            onClick={openForm}
+            id="course-edit"
+          >
+            <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z" />
+            <path
+              fillRule="evenodd"
+              d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z"
+            />
+          </svg>
+        )}
       </h3>
       <p className="course-title">{course.title}</p>
       <p className="course-meets">{course.meets}</p>
